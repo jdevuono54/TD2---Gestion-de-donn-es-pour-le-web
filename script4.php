643b7c79373621dea0306a8987bf4581bd0b14fc
Jacques <jacques.devuono@gmail.com>
2019-11-27 11:28:26 +0100

    PARTIE 4 OK

--- script4.php
+++ script4.php
@@ -27,4 +27,28 @@ $carte = Carte::find(28)->Commandes()->where("etat",">=",0)->where("montant",">"
 $items = Commande::find("9f1c3241-958a-4d35-a8c9-19eef6a4fab3")->Items()->where("tarif","<",5)->get();;
 //echo $items;
 
-//PARTIE 4
+// PARTIE 4
+$carte = Carte::has("Commandes",">",8)->get();
+//echo $carte;
+
+// PARTIE 5
+$carte = Carte::whereHas("Commandes", function ($query){
+    $query->has("items",">",3);
+})->get();
+//echo $carte;
+
+// PARTIE 6
+$commande = Commande::whereHas("items", function($query){
+    $query->where("item_id","=",2);
+})->get();
+//echo $commande;
+
+// PARTIE 7
+$carte = Carte::whereHas("Commandes", function($query){
+    $query->where("montant",">",30);
+})->get();
+//echo $carte;
+
+// PARTIE 8
+$commande = Commande::select()->whereNotNull("carte_id")->has("items",">",3)->get();
+//echo $commande;
